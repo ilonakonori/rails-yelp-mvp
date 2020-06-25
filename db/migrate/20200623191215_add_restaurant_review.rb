56d1af52cccd23908a26dee678fafdaaf12f0850
Ilona Kozenkovaite <kozenkovaite@gmail.com>
2020-06-25 19:45:11 +0200

    restaurants

---
+++ db/migrate/20200623191215_add_restaurant_review.rb
@@ -0,0 +1,6 @@
+class AddRestaurantReview < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :reviews, :restaurant, index: true
+    add_foreign_key :reviews, :restaurants
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create({
+  name: "Villa Rixfdorf",
+  address: "283 fake street Berlin",
+  category: "belgian"
+})
+Restaurant.create({
+   name: "Industry Standarts",
+  address: "89 fake street Berlin",
+  category: "french"
+})
+Restaurant.create({
+   name: "The Bird",
+  address: "101 fake street Berlin",
+  category: "japanese"
+})
+Restaurant.create({
+   name: "Grill Royal",
+  address: "13 fake street Berlin",
+  category: "chinese"
+})
+Restaurant.create({
+   name: "Lokal",
+  address: "57 fake street Berlin",
+  category: "italian"
+})
